82bf7a2c9c557d39c0bd42d6708cb251c303991e
materna-alm <materna-alm@materna.de>
2023-02-10 08:23:36 +0000

    Repository mit Vorlage initialisiert

---
+++ jse-demo/src/main/java/de/materna/jse/App.java
@@ -0,0 +1,16 @@
+package de.materna.jse;
+
+/**
+ * Hello world!
+ *
+ * @author fpientka
+ */
+
+public class App {
+    /**
+     * @param args command line arguments
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ jse-demo/src/test/java/de/materna/jse/AppTest.java
@@ -0,0 +1,22 @@
+package de.materna.jse;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ *
+ * @author fpientka
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     *
+     * @author fpientka
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
