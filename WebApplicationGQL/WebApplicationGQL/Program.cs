52b2a19c218faf4e2aa3c0627cef6ca0eefdd100
Mahmoud Elgohary <mahmoud.elgohary@materna.de>
2023-02-21 17:23:31 +0100

    allow cors from anywhere

--- WebApplicationGQL/WebApplicationGQL/Program.cs
+++ WebApplicationGQL/WebApplicationGQL/Program.cs
@@ -5,22 +5,20 @@ using GoldBeckLight.Repositories;
 using HotChocolate.Data.Neo4J;
 using System.Diagnostics;
 
-var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
-
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(name: MyAllowSpecificOrigins,
-                      builder =>
-                      {
-                          builder.WithOrigins("http://localhost:8080",
-                                              "http://www.contoso.com");
-                      });
-});
 IDriver driver = GraphDatabase.Driver(
               "neo4j+s://2725a56b.databases.neo4j.io",
               AuthTokens.Basic("neo4j", "rbAihX34NduwZaWL64EkKKwR_cYsf3UY5cMwJhqjidk"));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
 
 builder.Services.AddSingleton<IDriver>(driver)
     .AddScoped<IFloorRepository, FloorRepository>()
@@ -38,7 +36,7 @@ builder.Services.AddSingleton<IDriver>(driver)
                         .AddFiltering()
                         .AddProjections();
 var app = builder.Build();
-app.UseCors(MyAllowSpecificOrigins);
+app.UseCors("AllowAll");
 
 app.MapGraphQL("/graphql");
 
